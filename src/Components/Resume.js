6891271549d3698bba7737fa7f1d927e249bfc24
John McAvoy <mcavoyj5@students.rowan.edu>
2020-06-03 14:43:42 -0400

    Remove skills

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 class Resume extends Component {
   render() {
     if (this.props.data) {
-      var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function (education) {
         return (
           <div key={education.school}>
@@ -26,15 +25,6 @@ class Resume extends Component {
             </p>
             <p>{work.description}</p>
           </div>
-        );
-      });
-      var skills = this.props.data.skills.map(function (skills) {
-        var className = "bar-expand " + skills.name.toLowerCase();
-        return (
-          <li key={skills.name}>
-            <span style={{ width: skills.level }} className={className}></span>
-            <em>{skills.name}</em>
-          </li>
         );
       });
     }
@@ -65,23 +55,6 @@ class Resume extends Component {
           <div className="nine columns main-col">{work}</div>
         </div>
 
-        {/*
-        <div className="row skill">
-          <div className="three columns header-col">
-            <h1>
-              <span>Skills</span>
-            </h1>
-          </div>
-
-          <div className="nine columns main-col">
-            <p>{skillmessage}</p>
-
-            <div className="bars">
-              <ul className="skills">{skills}</ul>
-            </div>
-          </div>
-        </div>
-        */}
       </section>
     );
   }
