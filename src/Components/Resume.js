0f204cc1a4268882b8e1210cef9d1f6a6ce1f0ad
John McAvoy <mcavoyj5@students.rowan.edu>
2020-06-11 22:30:33 -0400

    Add resume logos

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,33 +5,50 @@ class Resume extends Component {
     if (this.props.data) {
       var education = this.props.data.education.map(function (education) {
         return (
-          <div key={education.school}>
-            <h3>{education.school}</h3>
-            <p className="info">
-              {education.degree} <span>&bull;</span>
-              <em className="date">{education.graduated}</em>
-            </p>
-            <p>{education.description}</p>
+          <div className="row" key={education.school.name}>
+            <div className="two columns">
+              <a href={education.school.website} target="_blank"><img src={education.school.logo}></img></a>
+            </div>
+            <div className="ten columns">
+              <h3>{education.school.name}</h3>
+              <p className="info">
+                {education.degree} <span>&bull;</span>
+                <em className="date">{education.graduated}</em>
+              </p>
+              <p>{education.description}</p>
+            </div>
           </div>
         );
       });
       var work = this.props.data.work.map(function (work) {
         return (
-          <div key={work.company}>
-            <h3>{work.company}</h3>
-            <p className="info">
-              {work.title}
-              <span>&bull;</span> <em className="date">{work.years}</em>
-            </p>
-            <p>{work.description}</p>
+          <div className="row" key={work.company}>
+            <div className="two columns">
+              <a href={work.website} target="_blank"><img src={work.logo}></img></a>
+            </div>
+            <div className="ten columns">
+              <h3>{work.company}</h3>
+              <p className="info">
+                {work.title}
+                <span>&bull;</span> <em className="date">{work.years}</em>
+              </p>
+              <p>{work.description}</p>
+            </div>
           </div>
         );
       });
-      var extra_circulars = this.props.data.extra_curriculars.map(function (extra_curricular) {
+      var extra_circulars = this.props.data.extracurriculars.map(function (
+        extracurricular
+      ) {
         return (
-          <div key={extra_curricular.name}>
-            <h3>{extra_curricular.name}</h3>
-            <p>{extra_curricular.description}</p>
+          <div className="row" key={extracurricular.name}>
+            <div className="two columns">
+              <a href={extracurricular.website} target="_blank"><img src={extracurricular.logo}></img></a>
+            </div>
+            <div className="ten columns">
+              <h3>{extracurricular.name}</h3>
+              <p>{extracurricular.description}</p>
+            </div>
           </div>
         );
       });
@@ -47,9 +64,7 @@ class Resume extends Component {
           </div>
 
           <div className="nine columns main-col">
-            <div className="row item">
-              <div className="twelve columns">{education}</div>
-            </div>
+            <div className="row item">{education}</div>
           </div>
         </div>
 
@@ -62,18 +77,16 @@ class Resume extends Component {
 
           <div className="nine columns main-col">{work}</div>
         </div>
-        
+
         <div className="row societies">
           <div className="three columns header-col">
             <h1>
-              <span>Extra-curriculars</span>
+              <span>Extracurriculars</span>
             </h1>
           </div>
 
           <div className="nine columns main-col">{extra_circulars}</div>
         </div>
-        
-
       </section>
     );
   }
